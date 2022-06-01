111d4004648dd33bf56cffaab4b8ea61d7ddcbc9
Everton Solon Tiba <evertonsolon@outlook.com>
2022-06-01 14:56:12 -0300

    Configuração de Muitos para Um

---
+++ Switch.Infra.Data/Config/PostagemConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Switch.Domain.Entities;
+
+namespace Switch.Infra.Data.Config
+{
+    public class PostagemConfiguration : IEntityTypeConfiguration<Postagem>
+    {
+        public void Configure(EntityTypeBuilder<Postagem> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.DataPublicacao);
+            builder.Property(x => x.DataPublicacao).IsRequired();
+            builder.Property(x => x.Texto).IsRequired().HasMaxLength(400);
+
+            builder.HasOne(x => x.Usuario).WithMany(x => x.Postagens);
+        }
+    }
+}
